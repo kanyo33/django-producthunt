449818b3fffeb4a645219fb37ef9ebbca31b06f3
test1 <n-k@live.ca>
2020-03-12 17:42:49 +0200

    connect views

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
 
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
